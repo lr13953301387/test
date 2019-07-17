b0ef87daadeb1fba6468d5d7bd2b1342612347bb
lr13953301387 <lr13953301387@163.com>
2019-07-17 11:15:07 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Hello extends React.Component
+{
+    render(){
+        return(
+            <div>dasdasdasdadasd</div>
+        );
+    }   
+}
+ReactDom.render(
+    <Hello/>,
+    document.getElementById('abc')
+);
+
+export default test
